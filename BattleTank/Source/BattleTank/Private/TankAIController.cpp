c252bfbb51fbe71c1e51184219b0409947e4a801
iamba <saenzsaul93@gmail.com>
2019-04-22 17:56:51 -0400

    Added a method to TankAIController that lets the AI tank find the player.

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -8,6 +8,8 @@ void ATankAIController::BeginPlay()
 	Super::BeginPlay();
 
 	auto AIControlledTank = GetAIControlledTank();
+	auto PlayerControlledTank = GetPlayerTank();
+
 	if (!AIControlledTank) 
 	{
 		UE_LOG(LogTemp, Warning, TEXT("TankAIController currently not possessing a tank."))
@@ -16,10 +18,26 @@ void ATankAIController::BeginPlay()
 	{
 		UE_LOG(LogTemp, Warning, TEXT("TankAIController now possessing %s"), *AIControlledTank->GetName());
 	}
+
+	if (!PlayerControlledTank) 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Where the fuck is the player?!"));
+	}
+	else 
+	{
+		UE_LOG(LogTemp, Warning, TEXT("I found %s"), *PlayerControlledTank->GetName());
+	}
 }
 
 ATank* ATankAIController::GetAIControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerTank = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerTank) { return nullptr; }
+	return Cast<ATank>(PlayerTank);
 }
 
