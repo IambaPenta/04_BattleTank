c3080a342619726ff37101f8bd3d4c4bcd013f5a
iamba <saenzsaul93@gmail.com>
2019-04-22 19:39:01 -0400

    Added some code to TankPlayerController's GetSightRayHitLocation method that finds the crosshair position in pixel coordinates.

--- BattleTank/Source/BattleTank/Public/TankPlayerController.h
+++ BattleTank/Source/BattleTank/Public/TankPlayerController.h
@@ -20,13 +20,18 @@ public:
 
 	virtual void Tick(float DeltaTime) override;
 
+private:
 	ATank* GetControlledTank() const;
 
 	//Start the tank moving the barrel so that a shot would hit where the crosshair intersects the world
 	void AimTowardsCrosshair();
 
-private:
-
 	//Return an OUT parameter, true if hit landscape
 	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.33333f;
 };
